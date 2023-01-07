ce96dff81c853d8f965a442e65250031a94e7a85
Аленин Андрей <aleninmailbox@gmail.com>
2023-01-08 00:18:09 +0300

    commit2.0

--- src/test/java/page/PurchasingTicketPage.java
+++ src/test/java/page/PurchasingTicketPage.java
@@ -5,19 +5,19 @@ import data.DataHelper;
 
 import static com.codeborne.selenide.Selenide.$;
 
-public class CardPage {
+public class PurchasingTicketPage {
     private final SelenideElement CardNumberField = $("//input[@placeholder='0000 0000 0000 0000']");
     private final SelenideElement MonthField = $("//input[@placeholder='08']");
     private final SelenideElement YearField = $("//input[@placeholder='22']");
     private final SelenideElement CardHolderField = $("//span[contains(text(),'Владелец')]");
     private final SelenideElement CVCCodeField = $("//input[@placeholder='999']");
 
-    public CardPage page (DataHelper.ValidHolderInfo info) {
+    public PurchasingTicketPage page (DataHelper.ValidHolderInfo info) {
         CardNumberField.setValue(info.getCardNumber());
         MonthField.setValue(info.getMonth());
         YearField.setValue(info.getYear());
         CardHolderField.setValue(info.getHolderName());
         CVCCodeField.setValue(info.getCVC());
-        return new CardPage();
+        return new PurchasingTicketPage();
     }
 }
