56a143d41b33fb3cfad6a07997006f7f7ab1012a
Аленин Андрей <aleninmailbox@gmail.com>
2023-01-08 00:27:41 +0300

    commit2.0

--- src/test/java/page/PurchaseTicketPage.java
+++ src/test/java/page/PurchaseTicketPage.java
@@ -5,19 +5,23 @@ import data.DataHelper;
 
 import static com.codeborne.selenide.Selenide.$;
 
-public class PurchasingTicketPage {
+public class PurchaseTicketPage {
     private final SelenideElement CardNumberField = $("//input[@placeholder='0000 0000 0000 0000']");
     private final SelenideElement MonthField = $("//input[@placeholder='08']");
     private final SelenideElement YearField = $("//input[@placeholder='22']");
     private final SelenideElement CardHolderField = $("//span[contains(text(),'Владелец')]");
     private final SelenideElement CVCCodeField = $("//input[@placeholder='999']");
 
-    public PurchasingTicketPage page (DataHelper.ValidHolderInfo info) {
+    private final SelenideElement PurchaseButton = $("//span[contains(text(),'Продолжить')]");
+
+
+    public PurchaseTicketPage page (DataHelper.ValidHolderInfo info) {
         CardNumberField.setValue(info.getCardNumber());
         MonthField.setValue(info.getMonth());
         YearField.setValue(info.getYear());
         CardHolderField.setValue(info.getHolderName());
         CVCCodeField.setValue(info.getCVC());
-        return new PurchasingTicketPage();
+        PurchaseButton.click();
+        return new PurchaseTicketPage();
     }
 }
--- src/test/java/test/PurchaseTicketTest.java
+++ src/test/java/test/PurchaseTicketTest.java
@@ -1,15 +1,17 @@
 package test;
 
 import org.junit.jupiter.api.Test;
+import page.PurchaseTicketPage;
 import page.WelcomePage;
 
 import static com.codeborne.selenide.Selenide.open;
 
-public class PurchasingTicketTest {
+public class PurchaseTicketTest {
     @Test
     void shouldPurchasingTicketFromCard() {
         open("http://localhost:8080");
         var welcomePage = new WelcomePage();
+        var purchasingPage = new PurchaseTicketPage();
     }
 
 }
